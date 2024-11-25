1a8371c32c74883b55327dcbe92e23d2c18aa187
GianpAbbrescia95 <g.abbrescia@icloud.com>
2024-11-25 12:05:41 +0100

    secondo

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-function dev(list, language, continent) {
-    return list.filter(dev => dev.language === language && dev.continent === continent).length;
+// function dev(list, language, continent) {
+//     return list.filter(dev => dev.language === language && dev.continent === continent).length;
+//   }
+  
+//   let list = [
+//     { firstName: 'Davide', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//     { firstName: 'Michele', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//     { firstName: 'Gianpietro', lastName: 'L.', country: 'Italy', continent: 'Asia', age: 29, language: 'HTML' },
+//     { firstName: 'Ettore', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+//   ];
+  
+//   let result = dev(list, 'JavaScript', 'Europe');
+//   console.log(result);  
+
+//   function dev(list, language, continent) {
+//     return list.filter(dev => dev.language === language && dev.continent === continent).length;
+//   }
+  
+//   function getUserInputAndCount() {
+//     let continent = prompt("Inserisci il continente (es. Europe, Asia, Oceania):");
+//     let language = prompt("Inserisci il linguaggio di programmazione (es. JavaScript, HTML, CSS):");
+  
+//     let result = dev(list, language, continent);
+//     alert(`Numero di sviluppatori con ${language} provenienti da ${continent}: ${result}`);
+//   }
+  
+//   getUserInputAndCount();
+  
+
+function addSaluto(list) {
+    return list.map(dev => {
+      dev.saluto = `Ciao ${dev.firstName}, cosa ti piace di più di${dev.language}?`;
+      return dev; 
+    });
   }
   
   let list = [
-    { firstName: 'Davide', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
-    { firstName: 'Michele', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
-    { firstName: 'Gianpietro', lastName: 'L.', country: 'Italy', continent: 'Asia', age: 29, language: 'HTML' },
-    { firstName: 'Ettore', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+    { firstName: 'Gianpietro', lastName: 'I.', country: 'Argentina', continent: 'Europe', age: 28, language: 'Java' },
+    { firstName: 'Ruggero', lastName: 'X.', country: 'Croatia', continent: 'America', age: 28, language: 'Python' },
+    { firstName: 'Sergio', lastName: 'U.', country: 'United States', continent: 'Europe', age: 28, language: 'Ruby' }
   ];
   
-  let result = dev(list, 'JavaScript', 'Europe');
-  console.log(result);  
-
-  function dev(list, language, continent) {
-    return list.filter(dev => dev.language === language && dev.continent === continent).length;
-  }
+  let updatedList = addSaluto(list);
   
-  function getUserInputAndCount() {
-    let continent = prompt("Inserisci il continente (es. Europe, Asia, Oceania):");
-    let language = prompt("Inserisci il linguaggio di programmazione (es. JavaScript, HTML, CSS):");
-  
-    let result = dev(list, language, continent);
-    alert(`Numero di sviluppatori con ${language} provenienti da ${continent}: ${result}`);
-  }
-  
-  getUserInputAndCount();
+  console.log(updatedList);
   
